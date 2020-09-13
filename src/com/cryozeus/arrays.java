71f21c7a06ba22bcf83f12caadfbe71c885129b3
CryoZEUS <yashbhardwaj911@gmail.com>
2020-09-13 15:38:23 +0530

    added arrays

--- src/com/cryozeus/arrays.java
+++ src/com/cryozeus/arrays.java
@@ -1,2 +1,70 @@
-package com.cryozeus;public class arrays {
+package com.cryozeus;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class arrays {
+
+    public static void main(String[] args) {
+
+        /*Arrays are just boxes of memory that allow you to assign names to multiple values*/
+        //Setting aside 10 spaces for our numbers :
+        int[] a1 = new int[10];
+        a1[0] = 1;
+        //filling the value '2' in all the spots :
+        Arrays.fill(a1, 2);
+        //Getting values :
+        System.out.println(a1[0]);
+        //Getting array size :
+        System.out.println(a1.length);
+        //Creating a string array and adding values simultaneously :
+        String[] a2 = {"one", "two"};
+        //Generating an array :
+        /*IntStream is discussed later.
+        * also, rangeClosed has the latter value included.*/
+        int[] oneTo10 = IntStream.rangeClosed(1,10).toArray();
+
+        /*since the following is a single line statement, we don't need curly brackets.*/
+        for(int x: oneTo10) System.out.println(x);
+
+        //Finding values :
+        /*outputs the index at which the value is found.*/
+        System.out.println(Arrays.binarySearch(oneTo10, 9));
+
+        //Creating multi-dimensional arrays :
+        int a3[][] = new int[2][2];
+        /*We can create and initialize at the same time.*/
+        String[][] a4 = {{"00", "10"},
+                         {"01", "11"}};
+        System.out.println(a4[1][1]);
+
+        //3D Array :
+        String a5[][][] = {{{"000"}, {"100"}, {"200"}, {"300"}},
+                            {{"010"}, {"110"}, {"210"}, {"310"}},
+                            {{"020"}, {"120"}, {"220"}, {"320"}}};
+
+        System.out.println(a5[2][3][0]);
+
+        //Copying one array into another :
+        int a6[] = {1, 2, 3};
+        int a7[] = Arrays.copyOf(a6, 3);    //3 = num of vlaues you wanna copy
+        //Compare a6 with a7
+        System.out.println(Arrays.equals(a6, a7));
+
+        //Sorting arrays :
+        int a8[] = {3, 2, 1};
+        Arrays.sort(a8);
+        /*convert the array into a string we can output : */
+        System.out.println(Arrays.toString(a8));
+
+
+
+
+
+
+
+
+
+
+    }
 }
--- src/com/cryozeus/str_builders_buffers.java
+++ src/com/cryozeus/str_builders_buffers.java
@@ -4,6 +4,33 @@ public class str_builders_buffers {
 
     public static void main(String[] args) {
 
+        /*If we are going to make many string changes,
+        * we may want to use a 'string builder'
+        *
+        * A 'String Buffer' on the other hand, is used whenever you are using threads
+        *
+        * In case of threads, just replace 'StringBuilder' below with 'StringBuffer*/
+
+        StringBuilder sb = new
+                StringBuilder("I'm a string Builder haha!");
+        /*Getting length, size and everything else we could get with strings : */
+        System.out.println(sb.length());
+        //Getting size :
+        System.out.println(sb.capacity());
+        //Appending a primitive or a string to a String Builder :
+        sb.append(" YEAH!");
+        //inserting at a specific index :
+        System.out.println(sb.insert(6, "Big "));
+        //Replacing at a specific index :
+        System.out.println(sb.replace(6,9, "wig"));
+        //Extracting substrings :
+        System.out.println(sb.substring(6, 10));
+        //Delete substring :
+        System.out.println(sb.delete(6, 10));
+        //Getting char at a specific index :
+        System.out.println(sb.charAt(4));
+        //Getting index for the string we type :
+        System.out.println(sb.indexOf("Yeah"));
 
 
 
